d120f38f51576f79360c507e94fdbb47b7d5ab8e
huanglipang <lh5jv@virginia.edu>
2020-12-06 23:44:47 -0500

    update description

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,30 @@ class App extends Component {
     render() {
         return (
             <div>
+                <h3>Search Million Rows</h3>
+                <div id="description">
+                    <span>
+                        This is a demo for searching strings from 2 million rows
+                        in just few milliseconds. The data source is from{" "}
+                    </span>
+                    <a
+                        href="https://www.kaggle.com/therohk/urban-dictionary-words-dataset/version/1"
+                        target="_blank"
+                    >
+                        urban-dictionary-words-dataset
+                    </a>
+                    <span> and the database is hosted on </span>
+                    <a href="https://www.mongodb.com/" target="_blank">
+                        Mongodb Atlas
+                    </a>
+                    .<span> For source code, please check this </span>
+                    <a
+                        href="https://github.com/HuangLiPang/million-rows"
+                        target="_blank"
+                    >
+                        repository
+                    </a>
+                </div>
                 <SearchInput
                     className="search-input"
                     onChange={this.searchUpdated}
